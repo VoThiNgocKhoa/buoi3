498df3c52c08806e5b872689d176a580bb0dc4ac
VoThiNgocKhoa <vongockhoa310104@gmail.com>
2024-09-15 03:14:39 +0700

    Update so_nguyen_duong_N.py

--- so_nguyen_duong_N.py
+++ so_nguyen_duong_N.py
@@ -7,8 +7,8 @@ Created on Sun Aug 25 13:47:18 2024
 
 N=int(input("nhap vao so nguyen duong co 2 chu so="))
 if 9<N<100:
-    hang_chuc = N % 10
-    hang_don_vi = N // 10
+    hang_chuc = N // 10
+    hang_don_vi = N % 10
     tong_cac_so_N = hang_chuc + hang_don_vi
     print(f"tong cac chu so cua N la:{ tong_cac_so_N}") 
 else:
